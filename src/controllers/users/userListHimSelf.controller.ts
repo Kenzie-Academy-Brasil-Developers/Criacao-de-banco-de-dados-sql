a832a2e2499a9c0510b994ee1f41b7cbeb843258
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-06 21:42:56 -0300

    Build: userList HimSelf route

---
+++ src/controllers/users/userListHimSelf.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import userListHimSelfService from "../../services/users/userListHimSelf.service";
+
+const userListHimSelfController = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params;
+
+    const user = await userListHimSelfService(id);
+
+    return res.status(200).send(user);
+  } catch (err) {
+    if (err instanceof Error) {
+      return res.status(400).send({
+        error: err.name,
+        message: err.message,
+      });
+    }
+  }
+};
+
+export default userListHimSelfController;
---
+++ src/services/users/userListHimSelf.service.ts
@@ -0,0 +1,15 @@
+import { AppDataSource } from "../../data-source";
+import { User } from "../../entities/user.entity";
+
+
+const userListHimSelfService = async (id : any) => {
+  const userRepository = AppDataSource.getRepository(User);
+
+  const users = await userRepository.find();
+
+  const account = users.find((user) => user.id === id);
+
+  return account;
+};
+
+export default userListHimSelfService;
