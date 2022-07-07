0af4cf0809ffb20655bc00c74677e6e15e5a686c
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-07 16:58:10 -0300

    Update: create service

--- src/services/users/userCreate.service.ts
+++ src/services/users/userCreate.service.ts
@@ -3,7 +3,12 @@ import { AppDataSource } from "../../data-source";
 import { User } from "../../entities/user.entity";
 import bcrypt from "bcrypt";
 
-const userCreateService = async ({ name, email, password,age }: IUserCreate) => {
+const userCreateService = async ({
+  name,
+  email,
+  password,
+  age,
+}: IUserCreate) => {
   const userRepository = AppDataSource.getRepository(User);
 
   const users = await userRepository.find();
@@ -18,13 +23,22 @@ const userCreateService = async ({ name, email, password,age }: IUserCreate) => 
   user.name = name;
   user.email = email;
   user.password = bcrypt.hashSync(password, 10);
-  user.age = age
-  user.created_at = new Date()
-  user.updated_at = new Date()
+  user.age = age;
+  user.created_at = new Date();
+  user.updated_at = new Date();
   userRepository.create(user);
   await userRepository.save(user);
 
-  return user;
+  const returnUser = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    age: user.age,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
+
+  return returnUser;
 };
 
 export default userCreateService;
