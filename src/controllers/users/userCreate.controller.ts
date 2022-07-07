afb4c1578f1411caed757cb388da0e4eb1f8f743
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-06 21:42:22 -0300

    Build: create user route

---
+++ src/controllers/users/userCreate.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import userCreateService from '../../services/users/userCreate.service'
+
+const userCreateController = async (req: Request, res: Response) => {
+
+    try {
+
+        const {name, email, password,age} = req.newUser
+
+        const newUser =  await userCreateService({name, email,password,age})
+        
+        return res.status(201).send(newUser)
+
+    } catch (err) {
+
+        if (err instanceof Error) {
+
+            return res.status(400).send({
+                "error": err.name,
+                "message": err.message
+            })
+        }
+    }
+}
+
+export default userCreateController
