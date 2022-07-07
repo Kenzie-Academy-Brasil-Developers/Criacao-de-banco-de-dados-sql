05196a4c7d13533cf75933ff4645e1bfdeb5ffdb
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-07 16:59:37 -0300

    Updated: interface IUserUpdated add

--- src/interfaces/user/index.ts
+++ src/interfaces/user/index.ts
@@ -4,3 +4,11 @@ export interface IUserCreate {
   password: string;
   age: number;
 }
+
+
+export interface IUserUpdated {
+  name: string;
+  email: string;
+  password: string;
+  age: number;
+}
