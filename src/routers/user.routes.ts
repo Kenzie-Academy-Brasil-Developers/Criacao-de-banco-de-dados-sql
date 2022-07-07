8ee4414de98c7481081560bd43296fc8cbcd7955
leonardo moya fernandes <leonardomoya03@gmail.com>
2022-07-07 16:59:11 -0300

    Update: userupdated route add

--- src/routers/user.routes.ts
+++ src/routers/user.routes.ts
@@ -6,7 +6,7 @@ import userCreateController from "../controllers/users/userCreate.controller";
 import userListController from "../controllers/users/userList.controller";
 import userListHimSelfController from "../controllers/users/userListHimSelf.controller";
 import userDeleteSelfController from "../controllers/users/userDeleteSelf.controller";
-// import userUpdateController from "../controllers/users/userUpdatePassword.controller";
+ import userUpdateController from "../controllers/users/userUpdate.controller";
 
 import { userCreateSchema } from "../middlewares/validateUserCreate.middleware";
 import { validateUserCreate } from "../middlewares/validateUserCreate.middleware";
@@ -15,7 +15,7 @@ routes.post("/users",validateUserCreate(userCreateSchema), userCreateController)
 routes.get("/users", userListController);
 routes.get("/users/:id", userListHimSelfController);
 routes.delete("/users/:id", userDeleteSelfController);
-// routes.patch("/users/me/updatePassword", userUpdateController);
+routes.patch("/users/:id", userUpdateController);
 
 export default routes;
 
